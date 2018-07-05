5fe5d8ed79f41178774864fd3b7e4e77735ecac7
Huy Nguyen <huy.ng1012@gmail.com>
2018-07-05 14:07:54 +0700

    - Initialize project

---
+++ GhostTypeExample/GhostTypeExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GhostTypeExample
+//
+//  Created by Huy Nguyễn on 7/5/18.
+//  Copyright © 2018 Huy Nguyễn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
